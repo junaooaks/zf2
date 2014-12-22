26aed7ac433f0900c84bb9401022e46ace739077
junior oaks <junaooaks@gmail.com>
2014-12-22 19:15:43 -0200

    pequeno projeto

---
+++ module/Cliente/src/Cliente/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Cliente\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function fooAction()
+    {
+        // This shows the :controller and :action parameters in default route
+        // are working when you browse to /module-specific-root/skeleton/foo
+        return array();
+    }
+}
