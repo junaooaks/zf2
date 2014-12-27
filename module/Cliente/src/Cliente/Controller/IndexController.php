81343410bf688d4f24cea3859050549f63fb6a9b
junior oaks <junaooaks@gmail.com>
2014-12-27 10:54:26 -0200

    paginação erro no module.conf

--- module/Cliente/src/Cliente/Controller/IndexController.php
+++ module/Cliente/src/Cliente/Controller/IndexController.php
@@ -2,7 +2,9 @@
 namespace Cliente\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController,
-    Zend\View\Model\ViewModel;
+    Zend\View\Model\ViewModel,
+    Zend\Paginator\Paginator,
+    Zend\Paginator\Adapter\ArrayAdapter;
 
 class IndexController extends AbstractActionController
 {
@@ -16,8 +18,16 @@ class IndexController extends AbstractActionController
        //buscar todos os dados da tabela cliente
        $dados = $repo->findAll();
        
+       //pegar o parametro da rota da pagina
+       $page = $this->params()->fromRoute('page');
+       
+       //criar uma paginação
+       $paginator = new Paginator(new ArrayAdapter($dados));
+       $paginator->setCurrentPageNumber($page);
+       $paginator->setDefaultItemCountPerPage(1);
+       
         //return array(array('dados'=>$dados));
-       return new ViewModel(array('dados' => $dados));
+       return new ViewModel(array('dados' => $paginator,'page'=>$page));
     }
 
     public function formAction()
